13f4f749cbdd04c407fd99169c0560ac6434b7b9
Lajkó Gege <127200655+lajkogege@users.noreply.github.com>
2024-09-09 09:49:03 +0200

    lotto

---
+++ src/modell/MindentTudoGep.java
@@ -0,0 +1,43 @@
+package modell;
+
+public class MindentTudoGep {
+
+    private int[] lottoSzelveny;
+    private int tottoSzelveny;
+    private String[] dalok;
+
+    public MindentTudoGep() {
+
+    }
+
+    public MindentTudoGep(String dalok) {
+
+    }
+
+    public MindentTudoGep(String[] dalok) {
+
+    }
+
+    public void ujraindit() {
+
+    }
+
+    public String generalLottoSzelveny() {
+
+        return " ";
+
+    }
+    
+    public String generalTottoSzelveny(){
+        return "";
+    }
+    
+    public String kerDal(){
+        return "";
+    }
+    
+    public void ujDal(String dal){
+        
+    }
+
+}
