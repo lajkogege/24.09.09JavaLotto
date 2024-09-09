ad5bbfbb581b47838e6b76400129b3e89c7162a2
Lajkó Gege <127200655+lajkogege@users.noreply.github.com>
2024-09-09 11:39:17 +0200

    ora vege

--- src/main/Program.java
+++ src/main/Program.java
@@ -1,10 +1,12 @@
 package main;
 
+import modell.MindentTudoGep;
+
 public class Program {
 
     
     public static void main(String[] args) {
-       
+        MindentTudoGep MindentTudoGep = MindentTudoGep;
     }
     
 }
--- src/modell/MindentTudoGep.java
+++ src/modell/MindentTudoGep.java
@@ -1,5 +1,9 @@
 package modell;
 
+import java.util.Arrays;
+import java.util.Random;
+import java.util.Scanner;
+
 public class MindentTudoGep {
 
     private int[] lottoSzelveny;
@@ -7,15 +11,18 @@ public class MindentTudoGep {
     private String[] dalok;
 
     public MindentTudoGep() {
+        lottoSzelveny = new int[5];
+        dalok = new String[10];
 
     }
 
     public MindentTudoGep(String dalok) {
-
+        this();
     }
 
     public MindentTudoGep(String[] dalok) {
-
+        this();
+        this.dalok = dalok;
     }
 
     public void ujraindit() {
@@ -23,21 +30,53 @@ public class MindentTudoGep {
     }
 
     public String generalLottoSzelveny() {
+        Random random = new Random();
+        String szelveny = "";
+        for (int i = 0; i < 4; i++) {
+            lottoSzelveny[i] = random.nextInt();
+            szelveny += lottoSzelveny[i];
+            System.out.println(lottoSzelveny[i]);
+            // lottoSzelveny[i].append;
+        }
+        return szelveny;
 
-        return " ";
+    }
 
+    public String kerLottoSzelveny() {
+        return "1,2,3,4,5";
     }
-    
-    public String generalTottoSzelveny(){
-        return "";
+
+    public void generalTottoSzelveny() {
+        this.tottoSzelveny();
     }
-    
-    public String kerDal(){
-        return "";
+
+    public String kerTottoSzelveny() {
+        String s = "";
+        s += "1 2 3 ... 13 13+1";
+        s += "x 1 2 ... x 1";
     }
-    
-    public void ujDal(String dal){
-        
+
+    public String[] kerDal() {
+        //Scanner src =new Scanner(System.in);
+        Scanner src = new Scanner(System.in);
+        dalok[0] = "Valami";
+        dalok[1] = "Izee";
+        dalok[2] = "AZZZ";
+        for (int i = 0; i < 10; i++) {
+            dalok[i]=src.nextLine();
+        }
+        return dalok[i];
+    }
+
+//    private String beker(String szoveg) {
+//        System.out.println(szoveg);
+//        return szoveg;
+//    }
+    public void ujDal(String dal) {
+        for (int i = 3; i < 10; i++) {
+            Scanner src = new Scanner(System.in);
+            dalok[i] = src.toString();
+        }
     }
 
 }
